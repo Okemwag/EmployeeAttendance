2986fd587da40575275a58ef7372529fb4cc5f83
Okemwag <gabrielokemwa83@gmail.com>
2024-04-04 00:10:21 +0300

    Chore: Set-up project

---
+++ client/lib/api/attendance_api.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+class Attendance extends StatelessWidget {
+  const Attendance({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
